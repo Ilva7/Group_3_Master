2b419683a27adafbd854467f573ca3b9fd6a9b63
Ilva Norenberga <ilva.norenberga@gmail.com>
2021-10-11 22:04:18 +0300

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        rocknroll();
+    }
+    public static void rocknroll()
+    {
+        System.out.println("Rock and Roll");
+    }
+}
