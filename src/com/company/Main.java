daf1d2a3c9d9f36f5d9a5df59d04ac790a4263b7
AliseBaumane <alise.adamsone@gmail.com>
2021-10-12 18:42:11 +0300

    Commit Alise

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
         rocknroll();
+        alise();
     }
-    public static void rocknroll()
-    {
+
+    public static void rocknroll() {
         System.out.println("Rock and Roll");
     }
+
+    public static void alise() {
+        System.out.println("Alise");
+    }
+
 }
