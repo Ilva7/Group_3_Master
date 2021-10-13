c72bc5fccbe533c715da8ea215e0b674d5325f7b
ZaneProjuma <zaneprojuma@gmail.com>
2021-10-13 18:30:28 +0300

    Zane

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         rocknroll();
+        Zane();
     }
     public static void rocknroll()
     {
         System.out.println("Rock and Roll");
     }
+    public static void Zane()
+    {
+        System.out.println("Zane");
+    }
 }
+
+
