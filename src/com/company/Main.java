4c5665844023eaa45ded3fd23dfa2dfd281e787f
Andra2010 <a.mozga@inbox.lv>
2021-10-13 15:28:16 +0300

    Andra Branch added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         rocknroll();
+        andra();
     }
     public static void rocknroll()
     {
         System.out.println("Rock and Roll");
+    }
+    public static void andra()
+    {
+        System.out.println("Andra");
     }
 }
